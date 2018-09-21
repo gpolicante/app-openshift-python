37fb42748749198cd84d49549364089400570141
gabriel <gabrielpolicante22@gmail.com>
2018-09-21 16:14:15 -0300

    python example

---
+++ app.py
@@ -0,0 +1,8 @@
+from flask import Flask
+app = Flask(__name__)
+@app.route("/")
+def index():
+        return "LULA LIVRE e BOLSOMITO tudo JUNTO "
+if __name__ == '__main__':
+        app.run(debug=True,host="0.0.0.0")
+
